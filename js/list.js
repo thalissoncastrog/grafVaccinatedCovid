d8a8f525fa6511b94c8489d5f3b1cb430ff446d2
Thalisson Castro <adaothalisson@hotmail.com>
2021-08-22 12:59:46 -0300

    Dados que vão ser plotatos no gráfico svg.

---
+++ js/list.js
@@ -0,0 +1,21 @@
+let information = [
+    {
+        city: 'Juazeiro',
+        generalPopulation: 31410,
+        seniors: 23920,
+        seniorsInShelters: 33,
+        seniorsInstitucionalized: 22,
+        securityForces: 1536,
+        healthWorkers: 6660,
+        educationWorkers: 3751,
+        transportWorkers: 339,
+        urbanCleaningWorkers: 248, 
+        comorbidities: 11610,
+        quilombolas: 807,
+        pregnants: 10772,
+        pospartum: 338,
+        breastfeeding: 1868,
+        communication: 108,
+    },
+
+];
